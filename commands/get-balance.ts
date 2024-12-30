b240e6a6f171d592f1dc9f7f13660f9a857eddba
Celestine <ejioforcelestine77l@gmail.com>
2024-12-30 09:38:13 +0100

    added stuff

--- commands/get-balance.ts
+++ commands/get-balance.ts
@@ -1,24 +1,26 @@
-import { Connection, PublicKey } from '@solana/web3.js';
 import UserWallet from '../models/user-wallet.schema';
 import { getWalletBalance } from '../utils/wallet-infra';
-import { getAssociatedTokenAddress } from '@solana/spl-token';
-import { USDC_MINT } from '../utils/helper';
 
 export default async function getBalance(ctx) {
-  if (ctx.chat.type !== 'private') return;
+  try {
+    if (ctx.chat.type !== 'private') return;
 
-  const username = ctx.from?.username;
-  if (!username) {
-    ctx.reply('Please set a username in Telegram to use this bot.');
-    return;
+    const username = ctx.from?.username;
+    if (!username) {
+      ctx.reply('Please set a username in Telegram to use this bot.');
+      return;
+    }
+
+    const wallet = await UserWallet.findOne({ username });
+    if (!wallet) {
+      ctx.reply("You don't have a wallet yet. Use /start to create one.");
+      return;
+    }
+
+    const balance = await getWalletBalance(wallet.address);
+    ctx.reply(`Your wallet address is: ${wallet.address}\n` + `Your balance is: *${balance} USDC*`);
+  } catch (error) {
+    console.log(error)
+    console.log('ok')
   }
-
-  const wallet = await UserWallet.findOne({ username });
-  if (!wallet) {
-    ctx.reply("You don't have a wallet yet. Use /start to create one.");
-    return;
-  }
-
-  const balance = await getWalletBalance(wallet.address);
-  ctx.reply(`Your balance is: ${balance} USDC`);
 }
--- index.ts
+++ index.ts
@@ -24,111 +24,126 @@ export interface MyContext extends Context {
 // Initialize bot and APIs
 const bot = new Telegraf<MyContext>(env.TELEGRAM_BOT_TOKEN!);
 
-// Connect to MongoDB
-connectDb();
+// bot.catch((err, ctx) => {
+//   console.error('Bot error:', err);
+//   //@ts-expect-error
+//   if (err.response.description?.includes('bot was blocked by the user')) {
+//     // Log blocked user
+//     console.log(`User ${ctx.from?.id} has blocked the bot`);
+//     return;
+//   }
+// });
 
 // Start Command (in private chat)
 bot.command('start', async (ctx) => {
-  await start(ctx);
-});
-
-// Create Bet Command
-bot.command('bet', async (ctx) => {
-  await createBet(ctx);
-});
-
-// Balance Command
-bot.command('balance', async (ctx) => {
-  await getBalance(ctx);
-});
-
-// Join Command (in group chat)
-bot.command('join', async (ctx) => {
-  await joinBet(ctx);
-});
-
-// Vote Command
-bot.command('vote', async (ctx) => {
-  if (ctx.chat.type === 'private') return;
-
-  const username = ctx.from?.username;
-  if (!username) {
-    ctx.reply('Please set a username in Telegram to use this bot.');
-    return;
-  }
-
-  const [betId, optionIndex] = ctx.message.text.split('/vote')[1].trim().split(' ');
-  const bet = await Bet.findOne({ betId });
-  if (!bet) {
-    ctx.reply('Invalid bet ID.');
-    return;
-  }
-
-  if (!bet.participants.includes(username)) {
-    ctx.reply('You have not joined this bet.');
-    return;
-  }
-
-  const option = parseInt(optionIndex);
-  if (isNaN(option) || option < 0 || option >= bet.options.length) {
-    ctx.reply('Invalid option.');
-    return;
-  }
-
-  //   bet.votes.set(username, option);
-  await bet.save();
-
-  ctx.reply(`Your vote for "${bet.options[option]}" has been recorded.`);
-});
-
-// Resolve Command
-bot.command('resolve', async (ctx) => {
-  await resolveBet(ctx);
-});
-
-bot.on('message', async (ctx) => {
-  // Check if message contains bot mention
-  const botUsername = ctx.botInfo.username;
-  const mentionRegex = new RegExp(`@${botUsername}`);
-  //@ts-ignore
-  const inputText = ctx.update.message?.text;
-  if (inputText.startsWith('/')) return;
-  if (ctx.chat.type === 'private' || (mentionRegex.test(inputText) && ctx.chat.type === 'group')) {
-    console.log(ctx.update);
-    const input = inputText?.replace(mentionRegex, '').trim();
-    if (input) {
-      const { result: command } = await classifyCommand(input);
-      console.log(command);
-      switch (command) {
-        case 'balance':
-          console.log('getting balance');
-          return getBalance(ctx);
-        case 'bet':
-          console.log('betting');
-          return createBet(ctx);
-        case 'join':
-          return joinBet(ctx);
-        case 'resolve':
-          if (
-            //@ts-ignore
-            ctx.message.reply_to_message &&
-            //@ts-ignore
-            ctx.message.reply_to_message.from?.username === bot.botInfo?.username &&
-            //@ts-ignore
-            ctx.message.reply_to_message.chat.type === 'group'
-          ) {
-            // This is a reply to the bot's message
-            return resolveBet(ctx);
-          }
-        case 'history':
-          return fetchBetHistory(ctx);
-        default:
-          console.log('default response');
-          ctx.reply(command);
-      }
+  try {
+    await start(ctx).catch((error) => console.log('user has blocked bot'));
+  } catch (error) {
+    console.log(error);
+    if (error) {
+      // Handle blocked user case
+      console.log(`User has blocked the bot`);
+      // Optional: Remove user from your database or mark them as inactive
     }
   }
 });
+
+// // Create Bet Command
+// bot.command('bet', async (ctx) => {
+//   await createBet(ctx);
+// });
+
+// // Balance Command
+// bot.command('balance', async (ctx) => {
+//   await getBalance(ctx);
+// });
+
+// // Join Command (in group chat)
+// bot.command('join', async (ctx) => {
+//   await joinBet(ctx);
+// });
+
+// // Vote Command
+// bot.command('vote', async (ctx) => {
+//   if (ctx.chat.type === 'private') return;
+
+//   const username = ctx.from?.username;
+//   if (!username) {
+//     ctx.reply('Please set a username in Telegram to use this bot.');
+//     return;
+//   }
+
+//   const [betId, optionIndex] = ctx.message.text.split('/vote')[1].trim().split(' ');
+//   const bet = await Bet.findOne({ betId });
+//   if (!bet) {
+//     ctx.reply('Invalid bet ID.');
+//     return;
+//   }
+
+//   if (!bet.participants.includes(username)) {
+//     ctx.reply('You have not joined this bet.');
+//     return;
+//   }
+
+//   const option = parseInt(optionIndex);
+//   if (isNaN(option) || option < 0 || option >= bet.options.length) {
+//     ctx.reply('Invalid option.');
+//     return;
+//   }
+
+//   //   bet.votes.set(username, option);
+//   await bet.save();
+
+//   ctx.reply(`Your vote for "${bet.options[option]}" has been recorded.`);
+// });
+
+// // Resolve Command
+// bot.command('resolve', async (ctx) => {
+//   await resolveBet(ctx);
+// });
+
+// bot.on('message', async (ctx) => {
+//   // Check if message contains bot mention
+//   const botUsername = ctx.botInfo.username;
+//   const mentionRegex = new RegExp(`@${botUsername}`);
+//   //@ts-ignore
+//   const inputText = ctx.update.message?.text;
+//   if (inputText[0] === '/') return;
+//   if (ctx.chat.type === 'private' || (mentionRegex.test(inputText) && ctx.chat.type === 'group')) {
+//     const input = inputText?.replace(mentionRegex, '').trim();
+//     if (input) {
+//       const { result: command } = await classifyCommand(input);
+//       console.log(command);
+//       switch (command) {
+//         case 'balance':
+//           console.log('getting balance');
+//           return getBalance(ctx);
+//         case 'bet':
+//           console.log('betting');
+//           return createBet(ctx);
+//         case 'join':
+//           return joinBet(ctx);
+//         case 'resolve':
+//           if (
+//             //@ts-ignore
+//             ctx.message.reply_to_message &&
+//             //@ts-ignore
+//             ctx.message.reply_to_message.from?.username === bot.botInfo?.username &&
+//             //@ts-ignore
+//             ctx.message.reply_to_message.chat.type === 'group'
+//           ) {
+//             // This is a reply to the bot's message
+//             return resolveBet(ctx);
+//           }
+//         case 'history':
+//           return fetchBetHistory(ctx);
+//         default:
+//           console.log('default response');
+//           ctx.reply(command);
+//       }
+//     }
+//   }
+// });
 
 // Start the bot
 // bot
