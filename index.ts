bf6e1a0d57b0da2891e1ad912c001751a369d22a
Celestine <ejioforcelestine77l@gmail.com>
2025-01-04 12:56:40 +0100

    made it more conversational

--- index.ts
+++ index.ts
@@ -105,27 +105,28 @@ bot.on('message', async (ctx) => {
         case 'balance':
           console.log('getting balance');
           return getBalance(ctx);
-        // case 'bet':
-        //   console.log('betting');
-        //   return createBet(ctx);
-        // case 'join':
-        //   return joinBet(ctx);
-        // case 'resolve':
-        //   if (
-        //     //@ts-ignore
-        //     ctx.message.reply_to_message &&
-        //     //@ts-ignore
-        //     ctx.message.reply_to_message.from?.username === bot.botInfo?.username &&
-        //     //@ts-ignore
-        //     ctx.message.reply_to_message.chat.type === 'group'
-        //   ) {
-        //     // This is a reply to the bot's message
-        //     return resolveBet(ctx);
-        //   }
-        // case 'history':
-        //   return fetchBetHistory(ctx);
+        case 'bet':
+          console.log('betting');
+          return createBet(ctx);
+        case 'join':
+          return joinBet(ctx);
+        case 'resolve':
+          if (
+            //@ts-ignore
+            ctx.message.reply_to_message &&
+            //@ts-ignore
+            ctx.message.reply_to_message.from?.username === bot.botInfo?.username &&
+            //@ts-ignore
+            ctx.message.reply_to_message.chat.type === 'group'
+          ) {
+            // This is a reply to the bot's message
+            return resolveBet(ctx);
+          }
+        case 'history':
+          return fetchBetHistory(ctx);
         default:
-          const gameInfo = await getPredoGameInfo(input);
+          console.log('predo fun reply')
+          const gameInfo = await getPredoGameInfo(input, ctx.from?.username, ctx.chat.type);
           ctx.reply(gameInfo);
       }
     }
